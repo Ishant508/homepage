05270ffe339ae8f4f8802a3f620277c6515b2548
roz3x <oroz3x@gmail.com>
2019-09-29 15:22:52 -0400

    fixed

--- index.js
+++ index.js
@@ -4,21 +4,29 @@ window.onload =()=> {
 		return
 	}
 
-
-	if (localStorage.getItem("hist_xxx") == null) {
-		var empty_hist = document.createElement('div') 
-		empty_hist.innerHTML = "no prev data"
-		document.body.appendChild(empty_hist)
-	} else {
-		//showing
-		 
-	}
 	var input = document.getElementById("input")
 	var setbtn = document.getElementById("setbtn")
 	var getbtn = document.getElementById("getbtn")
 	var clear = document.getElementById("clear")
 	var history = document.getElementById("history")
 
+
+	if (localStorage.getItem("hist_xxx") == null) {
+		var empty_hist = document.createElement('div') 
+		empty_hist.innerHTML = "no prev data"
+		history.appendChild(empty_hist)
+	} else {
+		while (history.firstChild){
+			history.removeChild(history.firstChild)
+		}
+		var hist =  localStorage.getItem("hist_xxx").split(",")
+		for (var i=hist.length-1;i>0;i--){
+			var div = document.createElement('div')
+			div.innerHTML = "<a href=http://www.google.com/search?q="+hist[i]+">"+hist[i]+"</a>"
+			history.appendChild(div)
+		}
+	}
+	
 	setbtn.onclick =()=>{	 
 		var prev_hist = localStorage.getItem("hist_xxx")
 		localStorage.setItem("hist_xxx",prev_hist +","+input.value)
@@ -39,6 +47,7 @@ window.onload =()=> {
  
 	clear.onclick =()=>{
 		localStorage.removeItem("hist_xxx")
+		getbtn.click()
 	}
 }
 
